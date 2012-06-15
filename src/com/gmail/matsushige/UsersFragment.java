79faa7fdffc14dcec8695410282e80d6f23a1a66
Takashi SASAKI <takashi316@gmail.com>
2012-06-16 04:52:16 +0900

    UsersFragment inherits BasicFragment

--- src/com/gmail/matsushige/UsersFragment.java
+++ src/com/gmail/matsushige/UsersFragment.java
@@ -1,5 +1,7 @@
 package com.gmail.matsushige;
 
+import com.gmail.takashi316.lib.BasicFragment;
+
 import android.app.Activity;
 import android.app.Fragment;
 import android.database.sqlite.SQLiteDatabase;
@@ -10,40 +12,31 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class UsersFragment extends Fragment {
-
-	@Override
-	public void onAttach(Activity activity) {
-		// TODO Auto-generated method stub
-		super.onAttach(activity);
-	}
+public class UsersFragment extends BasicFragment {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+		this.setFragmentLayoutId(R.layout.users_fragment);
 		super.onCreate(savedInstanceState);
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.users_fragment, container, false);
-		assert view != null;
+		View view = super.onCreateView(inflater, container, savedInstanceState);
 
-		Button buttonResetUsers = (Button) view
-				.findViewById(R.id.buttonResetUsers);
-		buttonResetUsers.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				UsersDatabaseHelper users_database_helper = new UsersDatabaseHelper(
-						v.getContext());
-				SQLiteDatabase sqlite_database = users_database_helper
-						.getWritableDatabase();
-				sqlite_database.close();
-			}// onClick
-		});
-
-		// return super.onCreateView(inflater, container, savedInstanceState);
+		this.setButtonOnClickListener(R.id.buttonResetUsers,
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						UsersDatabaseHelper users_database_helper = new UsersDatabaseHelper(
+								v.getContext());
+						SQLiteDatabase sqlite_database = users_database_helper
+								.getWritableDatabase();
+						sqlite_database.close();
+					}// onClick
+				});// onClickListener
 		return view;
-	}
+	}// onCreateView
+
 }// UserFragment
