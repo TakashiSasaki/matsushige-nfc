cd5c852a44f408e0c43612a0957629e03cb6193e
Takashi SASAKI <takashi316@gmail.com>
2012-06-19 11:42:06 +0900

    refactoring

--- src/com/gmail/takashi316/sandbox/HelloAndroidActivity.java
+++ src/com/gmail/takashi316/sandbox/HelloAndroidActivity.java
@@ -1,15 +1,15 @@
 package com.gmail.takashi316.sandbox;
 
-
 import android.app.Activity;
 import android.os.Bundle;
 import com.gmail.matsushige.R;
 
 public class HelloAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.hello_main);
-    }
-}
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.hello_main);
+	}// onCreate
+}// HelloAndroidActivity
+
